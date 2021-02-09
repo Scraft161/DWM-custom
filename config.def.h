02d14b141128574fb65eee25158d5a0880885f24
Scraft161 <Scraft161@gmail.com>
2021-02-09 16:56:28 +0100

    change default keybinds and add better tag switching

--- config.def.h
+++ config.def.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int gappx     = 10;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
@@ -11,7 +11,7 @@ static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "MesloLGS:size=10" };
-static const char dmenufont[]       = "MesloLGL:size=10";
+static const char dmenufont[]       = "MesloLGS:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -62,20 +62,24 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[]  = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *scriptcmd[] = { "echo", "WIP", NULL };
-static const char *termcmd[]   = { "st", NULL };
-static const char *fmcmd[]     = { "pcmanfm", NULL };
-static const char *exitcmd[]   = { "exitdwm", NULL };
-static const char *chatcmd[]   = { "discord", NULL };
+static const char *dmenucmd[]   = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *scriptcmd[]  = { "echo", "WIP", NULL };
+static const char *webcmd[]     = { "librewolf", NULL};
+static const char *termcmd[]    = { "st", NULL };
+static const char *fmcmd[]      = { "pcmanfm", NULL };
+static const char *exitcmd[]    = { "/home/scraft161/scripts/exitdwm", NULL };
+static const char *chatcmd[]    = { "discord", NULL };
+static const char *torrentcmd[] = { "qbittorrent", NULL};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_s,      spawn,          {.v = scriptcmd } },
+	{ STARTKEY,                     XK_W,      spawn,          {.v = webcmd } },
 	{ STARTKEY,                     XK_t,      spawn,          {.v = termcmd } },
-	{ STARTKEY,                     XK_r,      spawn,          {.v = fmcmd } },
+	{ STARTKEY,                     XK_f,      spawn,          {.v = fmcmd } },
 	{ STARTKEY,                     XK_d,      spawn,          {.v = chatcmd } },
+	{ STARTKEY,                     XK_q,      spawn,          {.v = torrentcmd} },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -91,8 +95,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_p,      setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
+	{ MODKEY,                       XK_F10,    view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_F10,    tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
@@ -100,17 +104,22 @@ static Key keys[] = {
 	{ MODKEY,                       XK_minus,  setgaps,        {.i = -5 } },
 	{ MODKEY,                       XK_equal,  setgaps,        {.i = +5 } },
 	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 10  } },
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
+	{ MODKEY,                       XK_F11,    viewtoleft,     {0} },
+	{ MODKEY,                       XK_F12,    viewtoright,    {0} },
+	{ MODKEY|ShiftMask,             XK_F11,    tagtoleft,      {0} },
+	{ MODKEY|ShiftMask,             XK_F12,    tagtoright,     {0} },
+	TAGKEYS(                        XK_F1,                      0)
+	TAGKEYS(                        XK_F2,                      1)
+	TAGKEYS(                        XK_F3,                      2)
+	TAGKEYS(                        XK_F4,                      3)
+	TAGKEYS(                        XK_F5,                      4)
+	TAGKEYS(                        XK_F6,                      5)
+	TAGKEYS(                        XK_F7,                      6)
+	TAGKEYS(                        XK_F8,                      7)
+	TAGKEYS(                        XK_F9,                      8)
 	{ MODKEY,                       XK_e,      spawn,          {.v = exitcmd } },
-	{ MODKEY|ShiftMask,             XK_e,      quit,           {1} },
+	{ MODKEY|ShiftMask,             XK_e,      quit,           {0} },
+	{ MODKEY|ShiftMask,             XK_r,      quit,           {1} },
 };
 
 /* button definitions */
